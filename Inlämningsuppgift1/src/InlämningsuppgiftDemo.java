0ac35f11d6d28199855ea0241a617be3e02f3cde
blhk <blhk@DESKTOP-0GVLFL0>
2017-10-03 09:03:22 +0200

    Uppdatering två.

--- Inlämningsuppgift1/src/InlämningsuppgiftDemo.java
+++ Inlämningsuppgift1/src/InlämningsuppgiftDemo.java
@@ -3,5 +3,8 @@ public class InlämningsuppgiftDemo {
 	public static void Main(String[] args) {
 		
 		System.out.println("hej");
+		System.out.println("hej");
+		System.out.println("hej");
+		
 	}
 }
